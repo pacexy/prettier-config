fac7073f7e13855923d687fc42ff762c9d9c49c3
pacexy <pacexy@gmail.com>
2024-04-17 19:36:47 +0800

    feat: add 'prettier-plugin-sort-json'

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,11 @@
 /** @type {import('prettier').Options} */
 export default {
+  plugins: ['prettier-plugin-sort-json'],
   semi: false,
   singleQuote: true,
   trailingComma: 'all',
+  // prettier-plugin-sort-json
+  ...{
+    jsonRecursiveSort: true,
+  },
 }
