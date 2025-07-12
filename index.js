061cca334392f2ccce64d2d36bbb2a0a1645acdf
pacexy <pacexy@gmail.com>
2025-07-12 19:30:44 +0800

    feat: offload sorting to eslint

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 /** @type {import('prettier').Options} */
 module.exports = {
   plugins: [
-    '@ianvs/prettier-plugin-sort-imports',
-    'prettier-plugin-sort-json',
-    'prettier-plugin-packagejson',
     'prettier-plugin-sql',
     'prettier-plugin-tailwindcss', // MUST come last - https://github.com/tailwindlabs/prettier-plugin-tailwindcss#compatibility-with-other-prettier-plugins
   ].map((m) => require.resolve(m)), // Resolve plugins manually to support pnpm v10. See https://github.com/pnpm/pnpm/issues/8378
   semi: false,
   singleQuote: true,
   trailingComma: 'all',
-  // options for prettier-plugin-sort-json
-  ...{
-    jsonRecursiveSort: true,
-  },
   // options for prettier-plugin-tailwindcss
   ...{
     tailwindFunctions: ['cva', 'cn'],
